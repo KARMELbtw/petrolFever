0726b1b1db18433f1de824279ed1622329777087
compactrex66 <compactrex66@gmail.com>
2023-11-30 19:59:29 +0100

    ok

--- Petrol Fever/Assets/Scripts/ChunkGeneration.cs
+++ Petrol Fever/Assets/Scripts/ChunkGeneration.cs
@@ -6,7 +6,34 @@ using UnityEngine;
 public class ChunkGeneration : MonoBehaviour
 {
     public GameObject cubePrefab;
+    public GameObject oil;
     public Transform startPosition;
+    public System.Random random = new System.Random();
+
+    public void generateOilVein(Vector3 startingPosition)
+    {
+        int veinMaxSize = 14;
+        int veinMinSize = 5;
+        int opposite = 0;
+        Instantiate(oil, startingPosition, Quaternion.identity);
+        Vector3 currentPosition = startingPosition;
+        for(int i = 0; i < random.Next(veinMinSize, veinMaxSize); i++) {
+            int whichDirection = random.Next(1,3);
+            opposite = random.Next(-1,2);
+
+            while(opposite == 0) {
+                opposite = random.Next(-1,2);
+            }
+
+            if(whichDirection == 1) {
+                currentPosition = new Vector3(currentPosition.x  + random.Next(-1,2), currentPosition.y, currentPosition.z);
+            } else if(whichDirection == 2) {
+                currentPosition = new Vector3(currentPosition.x, currentPosition.y + random.Next(-1, 2), currentPosition.z);
+            }
+            
+            Instantiate(oil, currentPosition, Quaternion.identity);
+        }
+    }
     
     // Start is called before the first frame update
     void Start()
@@ -23,7 +50,7 @@ public class ChunkGeneration : MonoBehaviour
                 {
                     currentPosition = new Vector3(k, i, j);
                     Instantiate(cubePrefab, currentPosition, Quaternion.identity, this.transform);
-                    Debug.Log("Wygenerowano klocek at: "+currentPosition);
+                    Debug.Log("Wygenerowano klocek at: " + currentPosition);
                 }
             }
         }
