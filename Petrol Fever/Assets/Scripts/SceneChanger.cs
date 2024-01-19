75651506e619c966396ad98778e1b99120780849
KARMELbtw <113383451+KARMELbtw@users.noreply.github.com>
2024-01-19 22:33:04 +0100

    zmiany

--- Petrol Fever/Assets/Scripts/SceneChanger.cs
+++ Petrol Fever/Assets/Scripts/SceneChanger.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour
 {
-    //funkcja zmieniająca scenę na tą zawartą w edytorze jako "sceneName" po kliknięciu guzika
-    public void ChangeScene(string sceneName)
+    //zadeklarowanie kamer
+    public Camera ChunksCamera;
+    public Camera TownCamera;
+    
+    //funkcja zmieniająca kamerę na drugą po kliknięciu guzika
+    public void ChangeScene()
     {
-        //komunikat o zmianie sceny na sceneName
-        Debug.Log("Changing scene to: " + sceneName);
-        //zmiana sceny na sceneName
-        SceneManager.LoadScene(sceneName);
+        Debug.Log("zmieniono scene");
+        ChunksCamera.gameObject.SetActive(false);
+        TownCamera.gameObject.SetActive(true);
     }
     
     // Start is called before the first frame update
