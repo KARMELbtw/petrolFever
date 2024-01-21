e326e2bd15262237c3434683966bd813e0f1bd77
Miszuk <aleksanderowsinski@interia.pl>
2024-01-21 19:33:25 +0100

    Naprawiłem kamere i tory
    Dodałem skrypty do kamery i ustawiłem jako główną
    oraz poprawiłem tory

--- Petrol Fever/Assets/Scripts/SceneChanger.cs
+++ Petrol Fever/Assets/Scripts/SceneChanger.cs
@@ -5,34 +5,20 @@ using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour
 {
-    
-    //zadeklarowanie kamery
-    public Camera Camera;
-    
-    //zadeklarowanie pozycji i roatcji kamery
+    //zadeklarowanie pozycji i rotacji kamery
     //miasto
-    public Vector3 TownCameraPosition = new Vector3(-34f, 36f, -23.5f);
-    public Vector3 TownCameraRotation = new Vector3(45f, 226.5f, 0f);
+    [SerializeField]
+    private Vector3 TownCameraPosition = new Vector3(-34f, 36f, -23.5f);
+    [SerializeField]
+    private Vector3 TownCameraRotation = new Vector3(45f, 226.5f, 0f);
     //chunki
-    public Vector3 ChunkCameraPosition = new Vector3(-20f, 41f, -20f);
-    public Vector3 ChunkCameraRotation = new Vector3(31.5f, 45f, 0f);
+    [SerializeField]
+    private Vector3 ChunkCameraPosition = new Vector3(-20f, 38f, -20f);
+    [SerializeField]
+    private Vector3 ChunkCameraRotation = new Vector3(31.5f, 45f, 0f);
     
-    //zadeklarowanie zmiennych
-    bool flag = false;
-    
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    //zadeklarowanie zmiennej do przechowywania informacji o tym czy kamera patrzy na chunki czy na miasto
+    bool isLookingAtChunk = false;
     
     void OnMouseDown()
     {
@@ -44,21 +30,21 @@ public class SceneChanger : MonoBehaviour
     void MoveCamera()
     {
         //zamiana kamer
-        if (flag)
+        if (isLookingAtChunk)
         {
-            Camera.transform.position = TownCameraPosition;
+            Camera.main.transform.position = TownCameraPosition;
             //nwm czemu ale trzeba dodać ten syf do rotacji bo inaczej ustawia roatcje na 0 360 0
-            Camera.transform.rotation = Quaternion.Euler(TownCameraRotation) * Quaternion.Euler(45, 226.5f, 0f);
+            Camera.main.transform.rotation = Quaternion.Euler(TownCameraRotation);
             Debug.Log("zmieniono widok na miasto");
-            flag = false;
+            isLookingAtChunk = false;
         }
         else
         {
-            Camera.transform.position = ChunkCameraPosition;
+            Camera.main.transform.position = ChunkCameraPosition;
             //tu działa bez tego syfu nwm czemu
-            Camera.transform.rotation = Quaternion.Euler(ChunkCameraRotation);
+            Camera.main.transform.rotation = Quaternion.Euler(ChunkCameraRotation);
             Debug.Log("zmieniono widok na chunki");
-            flag = true;
+            isLookingAtChunk = true;
         }
 
     }
