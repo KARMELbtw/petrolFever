81cc24a8328cfab654775eef3d622e6620b6b8cc
KARMELbtw <113383451+KARMELbtw@users.noreply.github.com>
2024-03-12 19:33:46 +0100

    cena robpy sie zmienia + balans cen

--- Petrol Fever/Assets/Scripts/sell.cs
+++ Petrol Fever/Assets/Scripts/sell.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class sell : MonoBehaviour
 {
     AudioSource audioSource;
+    public double sellMultiplier = 30;
+    private int i;
     public void sellOil() {
         if (GameManager.AmountOfOilNowSetGet <= 0) return;
-        GameManager.amountOfMoney += GameManager.AmountOfOilNowSetGet * 10;
+        GameManager.amountOfMoney += GameManager.AmountOfOilNowSetGet * (int)sellMultiplier;
         GameManager.AmountOfOilNowSetGet = 0;
         gameObject.GetComponent<AudioSource>().Play();
+        sellMultiplier = 30.0;
+    }
+    
+    void Update()
+    {
+        if (i >= 10)
+        {
+            sellMultiplier += 0.1;
+            i = 0;
+        }
     }
 }
