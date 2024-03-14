2e67631d5ad3ca9e62911e7d154b192d8b590793
Miszuk <aleksanderowsinski@interia.pl>
2024-03-14 18:35:48 +0100

    Dodane zmienne tutorial

---
+++ Petrol Fever/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    static public bool placedFirstDeer = false;
+    static public bool deerFoundOil = false;
+    static public bool placedFirstSilos = false;
+    static public bool firstOilDrilled = false;
+    static public bool soldFirstOil = false;
+}
