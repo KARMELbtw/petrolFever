fa70a5b57123eb8b91e39c556906b7a6e6f5ced5
compactrex66 <113365941+compactrex66@users.noreply.github.com>
2024-03-19 09:53:50 +0100

    zmienia siem

--- Petrol Fever/Assets/Scripts/sell.cs
+++ Petrol Fever/Assets/Scripts/sell.cs
@@ -13,10 +13,10 @@ public class sell : MonoBehaviour
     public void sellOil() {
         if (GameManager.AmountOfOilNowSetGet <= 0) return;
         GameManager.amountOfMoney += GameManager.AmountOfOilNowSetGet * (int)sellMultiplier;
-        GameManager.AmountOfOilNowSetGet = 0;
         gameObject.GetComponent<AudioSource>().Play();
         if(sellMultiplier - GameManager.AmountOfOilNowSetGet * 2 >= 2)
             sellMultiplier -= GameManager.AmountOfOilNowSetGet * 2;
+        GameManager.AmountOfOilNowSetGet = 0;
         TutorialManager.soldFirstOil = true;
     }
 
